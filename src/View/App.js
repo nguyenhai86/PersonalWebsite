20984ddba02b475056a154daae80d6ab6b5dea04
Nguyen Hai <87838271+nguyenhai1806@users.noreply.github.com>
2023-02-28 20:29:37 +0700

    done

---
+++ src/View/App.js
@@ -0,0 +1,49 @@
+import "./App.scss";
+import data from "./../data.json";
+import headerImage from "./../Images/header.jpg";
+
+function App() {
+  const handleClick = (url) => {
+    window.open(url);
+  };
+  const mapData = () => {
+    return data.map((value, index) => {
+      return (
+        <tr key={index} onClick={() => handleClick(value.url)}>
+          <td className="name">
+            <img
+              src={value.avatar}
+              alt={value.name + " avatar"}
+              className="avatar"></img>
+            <span>{value.name}</span>
+          </td>
+          <td>
+            <a href={value.url}>{value.name}</a>
+          </td>
+          <td>{value.description}</td>
+        </tr>
+      );
+    });
+  };
+  return (
+    <div>
+      <div className="header">
+        <img src={headerImage} alt="Header" />
+      </div>
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Website</th>
+              <th>Đường dẫn</th>
+              <th>Mô tả</th>
+            </tr>
+          </thead>
+          <tbody>{mapData()}</tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
